5a388b2a89539b746aa17fb92e94603ed51deee1
Marcelo Alves <marceloalves1295@gmail.com>
2021-11-27 14:02:22 -0300

    adicionar viewbinding e layoutcontainer

--- app/src/main/java/br/com/fakelibrary/presentation/MainActivityLayoutContainer.kt
+++ app/src/main/java/br/com/fakelibrary/presentation/MainActivityLayoutContainer.kt
@@ -1,8 +1,18 @@
 package br.com.fakelibrary.presentation
 
+import android.view.View
+import br.com.fakelibrary.databinding.ActivityMainBinding
+import kotlinx.android.extensions.LayoutContainer
+
 /**
  * @author RubioAlves
  * Created 27/11/2021 at 08:53
  */
-class MainActivityLayoutContainer {
+class MainActivityLayoutContainer(override val containerView: View, private val binding: ActivityMainBinding):LayoutContainer{
+
+    fun initLayout() = with(binding){
+
+    }
+
+
 }
